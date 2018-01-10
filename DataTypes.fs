b9f861ba4cbea3a75ba75b24bf45355f67ec5e45
Eddy Mogollon <eddymogs@gmail.com>
2018-01-09 23:53:38 -0500

    Modified DataTypes

--- DataTypes.fs
+++ DataTypes.fs
@@ -1,17 +1,17 @@
 (* Testing data types *)
 // And comments
 
-let my_int = 50
+let my_int:int = 50
 printf "%i\n" my_int
 
-let my_float = 50.50
+let my_float:float = 50.50
 printf "%.2f\n" my_float
 
-let my_char = 'F'
+let my_char:char = 'F'
 printf "This is %c#\n" my_char
 
-let my_string = "This is F#\n"
+let my_string:string = "This is F#\n"
 printf "%s" my_string
 
-let my_bool = true
+let my_bool:bool = true
 printf "True: %b\n" (my_bool)
