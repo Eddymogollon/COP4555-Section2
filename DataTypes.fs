87faa95b467d3e7ea6b2555de5353430d31b2687
Eddy Mogollon <eddymogs@gmail.com>
2018-01-09 23:50:46 -0500

    Added DataTypes

---
+++ DataTypes.fs
@@ -0,0 +1,17 @@
+(* Testing data types *)
+// And comments
+
+let my_int = 50
+printf "%i\n" my_int
+
+let my_float = 50.50
+printf "%.2f\n" my_float
+
+let my_char = 'F'
+printf "This is %c#\n" my_char
+
+let my_string = "This is F#\n"
+printf "%s" my_string
+
+let my_bool = true
+printf "True: %b\n" (my_bool)
---
+++ helloworld.fs
@@ -0,0 +1,2 @@
+System.Console.WriteLine "Hello Friend"
+printfn "Hello Friend"
